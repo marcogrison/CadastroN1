65d147a834240bda5e78895be1ff6d482989702e
Marco Grison <Marco.Grison@mercanet.com.br>
2022-10-12 22:27:04 -0300

    Add project files.

---
+++ Cadastro/Controllers/ProductsController.cs
@@ -0,0 +1,121 @@
+﻿using Cadastro.Interfaces;
+using Cadastro.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadastro.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductViewModelService _productViewModelService;
+        public ProductsController(IProductViewModelService productViewModelService)
+        {
+            _productViewModelService = productViewModelService;
+        }
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var list = _productViewModelService.GetAll();
+            return View(list);
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int id)
+        {
+            var viewModel = _productViewModelService.Get(id);
+            return View(viewModel);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ProductViewModel viewModel)
+        {
+            try
+            {         
+                if (ModelState.IsValid)
+                {
+                    _productViewModelService.Insert(viewModel);
+
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(viewModel);
+            }
+            catch
+            {
+                return View(viewModel);
+            }
+        
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var viewModel = _productViewModelService.Get(id);
+            return View(viewModel);
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, ProductViewModel viewModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _productViewModelService.Update(viewModel);
+
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(viewModel);
+            }
+            catch
+            {
+                return View(viewModel);
+            }
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var viewModel = _productViewModelService.Get(id);
+            return View(viewModel);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _productViewModelService.Delete(id);
+
+                    return RedirectToAction(nameof(Index));
+                }
+
+                var viewModel = _productViewModelService.Get(id);
+                return View(viewModel);
+            }
+            catch
+            {
+                var viewModel = _productViewModelService.Get(id);
+                return View(viewModel);
+            }
+        }
+    }
+}
---
+++ Cadastro/ViewModels/ProductViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Cadastro.Domain.Entities;
+using Cadastro.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks;
+
+namespace Cadastro.ViewModels
+{
+    public class ProductViewModel
+    {
+        [Key]
+        [Display(Name = "Código")]
+        [Required(ErrorMessage = "O código é requerido.")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome do Produto")]
+        [Required(ErrorMessage = "O nome é requerido.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Preço")]
+        [Required(ErrorMessage = "O preço é requerido.")]
+        public string Value { get; set; }
+
+        [Display(Name = "Disponível")]
+        public bool Active { get; set; }
+
+
+    }
+}
